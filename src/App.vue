4abd9573f91e2f2e34b71980c3ca5f11767a6250
vrungel <arsenyan.m.i20@gmail.com>
2021-12-02 10:48:47 +0300

    drower

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,52 @@
 <template> 
 
+  <v-app> 
+
+  <v-navigation-drawer app v-model="drawer"></v-navigation-drawer>  
+
+  <v-app-bar app dark color="primary"> 
+
+    <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon> 
+
+    <v-spacer></v-spacer> 
+
+    <v-toolbar-items class="hidden-sm-and-down"> 
+
+       <v-btn  text><v-icon left>mdi-account-check</v-icon>Вход</v-btn>  
+
+       <v-btn  text><v-icon left>mdi-account-multiple-plus</v-icon>Регистрация</v-btn>  
+
+       <v-btn  text>Создать</v-btn> 
+
+    </v-toolbar-items> 
+
+  </v-app-bar> 
+
+  <v-content> 
+
+    <router-view></router-view>  
+
+  </v-content> 
+
+  </v-app> 
 
 </template> 
-
- 
-
 <script> 
 
 export default { 
+  data() { 
+    return { 
 
-   
-
+      drawer: false 
+    } 
+  } 
 } 
-
+</script> <script> 
+export default { 
+  data() { 
+    return { 
+      drawer: false 
+    } 
+  } 
+} 
 </script> 
