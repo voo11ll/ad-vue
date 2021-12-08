0e02ab5cf3e01b5d356eec7dcc378c00f27dba6c
vrungel <arsenyan.m.i20@gmail.com>
2021-12-08 18:53:44 +0300

    7.1-7.2

--- src/components/Ads/NewAd.vue
+++ src/components/Ads/NewAd.vue
@@ -1,17 +1,82 @@
 <template> 
-	<v-container>
-		<v-layout row> 
-			<v-flex xs12>
-				<h1>NewAd</h1> 
-			</v-flex>
-		</v-layout> 
-	</v-container>
+    <v-container>
+        <v-layout row> 
+            <v-flex xs12 sm6 offset-sm3>
+                <h1 class="text--secondary mb-3 mt-3">Create Ad</h1>
+                <v-form v-model="valid" ref="form" validation>
+                  <v-text-field color="#800080"
+                     name="title" 
+                     label="Ad Title" 
+                     type="text" 
+                     v-model="title"
+                     :rules="[ v => !!v || 'Title is required']">
+                 </v-text-field>
+                  <v-textarea color="#800080"  
+                     name="description" 
+                     label="Ad Description" 
+                     type="text" 
+                     v-model="description"
+                     :rules="[ v => !!v || 'Description is required']"
+                     class="mb-3" >
+                 </v-textarea>
+                </v-form>
+
+                <v-layout  row> 
+                  <v-flex xs12>
+                      <v-btn class="mt-3" color="warning">
+                          Upload
+                         <v-icon right dark>mdi-cloud-upload</v-icon>
+                      </v-btn>
+
+                    </v-flex>
+                </v-layout>
+
+                <v-layout  row> 
+                 <v-flex xs12>
+                      <img src="https://i.pinimg.com/originals/bf/7c/9b/bf7c9b58e6e32ee5ee717bbf781371be.jpg" height="150" class="mt-3">
+                 </v-flex>
+                </v-layout>
+
+                <v-layout  row> 
+                  <v-flex xs12>
+                      <v-switch v-model="promo" color="#800080" label="Ad to Promo?"></v-switch>
+                    </v-flex>
+                </v-layout>
+
+                <v-layout  row> 
+                  <v-flex xs12>
+                      <v-btn color="success" @click="createAd">Create Ad</v-btn>
+                  </v-flex>
+                </v-layout>
+            </v-flex>
+        </v-layout> 
+    </v-container>
 </template>
+
+
+
+
 <script>
 export default {
-	data () { 
-		return {
-		} 	
-	}
+    data () { 
+        return {
+            valid: false,
+            title: "",
+            description: "",
+            promo: false
+        } 	
+    },
+    methods: {
+      createAd(){
+          if (this.$refs.form.validate()){
+          const ad = {
+         title: this.title,
+         desc: this.description,
+         promo: this.promo
+        }
+        console.log(ad)
+    }
+   }
+  }
 } 
 </script>
