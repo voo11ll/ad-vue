d749cabda36498b03d1b87b5a39e3f5ec1a85e3f
vrungel <arsenyan.m.i20@gmail.com>
2021-12-19 21:58:06 +0300

    12.1-12.3

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router/index'
 import store from './store'
+import fb from 'firebase'
 
 Vue.use(Router)
 Vue.config.productionTip = false
@@ -12,5 +13,19 @@ new Vue({
   vuetify,
   render: h => h(App),
   router:router,
-  store
+  store,
+  created(){
+    var firebaseConfig={ 
+      apiKey : "AIzaSyB6VXkXKlF97wCNRzSXc2OVpgmCN7ggbh4" , 
+      authDomain : "ad-vue-3a74f.firebaseapp.com" , 
+      projectId : "ad-vue-3a74f" , 
+      storageBucket : "ad-vue-3a74f.appspot.com" , 
+      messagingSenderId : "582047514078" , 
+      appId : "1: 582047514078: web: d859246530c684f921b14f" , 
+      measurementId : "G-L85J855MSL" 
+    };
+    // Initialize Firebase
+    fb.initializeApp(firebaseConfig);
+    fb.analytics();
+  }
 }).$mount('#app')
