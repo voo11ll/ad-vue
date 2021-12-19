579e99f964160f32fc4d9c229e0031de6842e0b6
vrungel <arsenyan.m.i20@gmail.com>
2021-12-19 22:39:56 +0300

    13.1-13.2

--- src/components/Auth/Registration.vue
+++ src/components/Auth/Registration.vue
@@ -72,7 +72,7 @@ export default {
                 email: this.email,
                 password: this.password
             }
-            console.log(user)
+            this.$store.dispatch('registerUser',user)
         }
     }
   }
--- src/store/user.js
+++ src/store/user.js
@@ -1,12 +1,30 @@
+import fb from 'firebase'
 export default {
     state:{
         user: null
     },
-    mutations:{},
-    actions:{},
+    mutations:{
+        setUser(state,payload) {
+            state.user = payload
+        }
+    },
+    actions:{
+        registerUser({commit},{email, password}){
+            fb.auth().createUserWithEmailAndPassword(email,password).then(response => {
+                console.log(response.user.uid)
+                commit('setUser', new User(response.user.uid))
+            })
+        }
+    },
     getters:{
         user(state){
             return state.user
         }
+    }
+}
+
+class User {
+    constructor(id) {
+        this.id = id
     }
 }
