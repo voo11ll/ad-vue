f5063e3672043d294ab24a3499ceea9a131853c2
vrungel <arsenyan.m.i20@gmail.com>
2022-01-25 00:18:13 +0300

    22.1-22.4

--- src/components/User/Orders.vue
+++ src/components/User/Orders.vue
@@ -1,29 +1,40 @@
 <template> 
-    <v-container>
-        <v-layout row> 
-            <v-flex xs12 sm6 offset-sm3>
-             <h1 class="text--secondary mb-3 mt-3">Orders</h1>
-                <v-list subheader two-line flat>		      
-                    <v-list-item v-for="order in orders" :key="order.id" >
-                       <v-list-item-action>
-                           <v-checkbox
-                              :input-value="order.done"
-                             color="#800080"
-                             @click="markDone(order)"
-                             ></v-checkbox>
-                        </v-list-item-action>
-                        <v-list-item-content>
-                          <v-list-item-title>{{ order.name }}</v-list-item-title>
-                          <v-list-item-subtitle>{{ order.phone }}</v-list-item-subtitle>
-                        </v-list-item-content>
-                 <v-list-item-action>
-                    <v-btn dark color="#800080" :to="'/ad/' + order.adId">Open</v-btn>
-                    </v-list-item-action>
-                   </v-list-item>
-             </v-list>
-           </v-flex>
-        </v-layout> 
-    </v-container>
+  <v-container>
+    <v-layout row> 
+      <v-flex xs12 sm2 offset-sm6 class="mt-5" v-if="loading">
+        <v-progress-circular 
+          :size="70"
+          :width="7"
+          color="#800080"
+          indeterminate>
+        </v-progress-circular>
+      </v-flex>
+      <v-flex xs12 sm6 offset-sm3 v-else-if="!loading && orders.length !== 0">
+       <h1 class="text--secondary mb-3 mt-3">Orders</h1>
+        <v-list subheader two-line flat>		      
+          <v-list-item v-for="order in orders" :key="order.id" >
+            <v-list-item-action>
+              <v-checkbox
+               :input-value="order.done"
+               color="#800080"
+               @click="markDone(order)">
+               </v-checkbox>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>{{ order.name }}</v-list-item-title>
+              <v-list-item-subtitle>{{ order.phone }}</v-list-item-subtitle>
+            </v-list-item-content>
+            <v-list-item-action>
+             <v-btn dark color="#800080" :to="'/ad/' + order.adId">Open</v-btn>
+            </v-list-item-action>
+          </v-list-item>
+        </v-list>
+      </v-flex>
+      <v-flex xs12 sm6 offset-sm3 v-else>
+        <h1 class="text--secondary">You hane no orders</h1>
+      </v-flex>
+    </v-layout> 
+  </v-container>
 </template>>
 
 
@@ -31,7 +42,7 @@
 
 <script>
 export default {
-    data () { 
+    /*data () { 
         return {
             orders: [
                {
@@ -58,13 +69,27 @@ export default {
             ]
 
         } 	
+    },*/
+    computed: {
+    loading () {
+      return this.$store.getters.loading
     },
-    methods: {
-	markDone(order) {
-		order.done = !order.done
-		console.log(order.done)
-	}
-}
+    orders () {
+      return this.$store.getters.orders
+    }
+  },
+  methods: {
+    markDone(order) {
+      this.$store.dispatch('markOrderDone', order.id)
+      .then(() => {
+        order.done = true
+      })
+      .catch(() => {})
+    }
+  },
+  created () {
+    this.$store.dispatch('fetchOrders')
+  }
 } 
 </script>
 
--- src/store/orders.js
+++ src/store/orders.js
@@ -1,14 +1,83 @@
+import fb from "firebase"
+class Order {
+    constructor(name, phone, adId, done = false, id = null) {
+        this.name = name
+        this.phone = phone
+        this.adId = adId
+        this.done = done
+        this.id = id
+    }
+}
 export default {
-	state: {},
-	mutations: {},
-	actions: {
-        async createOrder({commit},{name, phone, adId, ownerId}) {
-            await new Promise((resolve, reject) => {
-                setTimeout(() => {
-                    resolve()
-                }, 4000)
-            })
+    state: {
+        orders:[]
+    },
+    mutations: {
+        loadOrders (state, payload) {
+            state.orders = payload
         }
     },
-	getters: {}
+    actions: {
+        async createOrder({commit},{name, phone, adId, ownerId}) {
+            const order = new Order(name,phone,adId)
+            commit('clearError')
+            try {
+                await fb.database().ref(`/users/${ownerId}/orders`).push(order)
+
+            } catch (error){
+                commit('setError', error.message)
+                throw error
+            }
+        },
+        async fetchOrders ({commit, getters}) {
+            commit('setLoaging', true)
+            commit('clearError') 
+            const resultOrders = []
+            try {
+                 const fbVal = await fb.database().ref(`/users/${getters.user.id}/orders`).once('value')
+                 const orders = fbVal.val()
+                 if (orders !== null) {
+                     Object.keys(orders).forEach(key => {
+                         const order = orders[key]
+                         resultOrders.push(
+                         new Order(
+                         order.name,
+                         order.phone,
+                         order.adId,
+                         order.done,
+                         key))
+                        })
+                    }
+                 commit('loadOrders', resultOrders)
+                 commit('setLoaging', false)
+            } catch(error) {
+             commit('setError', error.message)
+             commit('setLoaging',false)
+             throw error
+            }
+        },
+        async markOrderDone ({commit, getters}, payload) {
+            commit('clearError')
+            try {
+                await fb.database().ref(`/users/${getters.user.id}/orders`).child(payload).update({
+                    done:true
+                })
+            } catch(error) {
+                commit('setError', error.message)
+                throw error
+            }
+        }
+    },
+    getters: {
+        doneOrders (state) {
+            return state.orders.filter(order => order.done)
+        },
+        undoneOrders(state) {
+            return state.orders.filter(order => !order.done)
+        },
+        orders (state, getters) {	
+            return getters.undoneOrders.concat(getters.doneOrders)
+        }
+    
+    }
 }
